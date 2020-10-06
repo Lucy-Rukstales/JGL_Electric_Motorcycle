7fdbf089bd156fa3de0ae76c9374d2be20b74174
gmh9 <44414331+gmh9@users.noreply.github.com>
2020-10-06 10:45:55 -0400

    Update Project_Script.m

--- Project_Script.m
+++ Project_Script.m
@@ -1,36 +1,40 @@
 clear
 clc
-% Tesla Model S Parameters from Table 2.1
-Prrated=270000;
-Trrated=440;
-r=0.315;%0.352
-ng=9.73;
-m=2155;
-A=177.2;
-B=1.445;
-C=0.354;
+
+Prrated=65280; % Rated Power in W
+Trrated=210*1.3563; %convert to Nm
+r=0.3; %radius in m - front tire (back tire is 0.32 m)
+ng=2;
+m=197; %197kg
+RiderMass=68; %approximately 150lb rider
+meq=m+RiderMass;
+A=18.3;
+B=0;
+C=0.0282*12.96;%(N/(km/h)^2) to (N/(m/s)^2)
 eta=0.97;
 
 wrrated=Prrated/Trrated;
 wmrated=wrrated/ng;
 vmrated=wmrated*r;
-dT=5e-4;
+dT=1e-3;
 
 v(1)=0;
 t(1)=0;
 dist(1)=0;
 n=1;
-while dist<(0.25*1609.34) %convert miles to meters
+while (dist<(0.25*1609.34)) %convert miles to meters
     if (v(n)<vmrated)
-        v(n+1)=v(n)+dT*(ng*eta*Trrated-r*(A+B*v(n)+C*(v(n))^2))/(r*m);
+        v(n+1)=v(n)+dT*(ng*eta*Trrated-r*(A+B*v(n)+C*(v(n))^2))/(r*meq);
     else
-        v(n+1)=v(n)+dT*(eta*((Prrated*r)/(v(n)))-r*(A+B*v(n)+C*(v(n))^2))/(r*m);
+        v(n+1)=v(n)+dT*(eta*((Prrated*r)/(v(n)))-r*(A+B*v(n)+C*(v(n))^2))/(r*meq);
     end
     t(n+1)=t(n)+dT;
-    dist(n)=trapz(v,t(1:length(v)));
+    dist=trapz(t(1:length(v)),v);
     n=n+1;
 end
-distFinal=trapz(v,t)
+distFinal=trapz(t,v)
 v=((v*3600)/1609.34); %convert m/s to mph
 plot(t,v)
+xlabel('Time')
+ylabel('Velocity')
 t(end) %time it takes to complete the quarter mile
